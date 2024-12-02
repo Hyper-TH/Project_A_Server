ab579724ce16a278d827855a5cbb32969e20415b
Twila [Hyper] Habab <twila.shayne.habab@outlook.com>
2024-12-02 15:23:33 +0000

    Connect to Redis Upstash

--- Program.cs
+++ Program.cs
@@ -1,6 +1,29 @@
+using StackExchange.Redis;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Redis Config
+var redisConfig = builder.Configuration.GetSection("Redis");
+var redisUrl = redisConfig["Url"];
+var redisToken = Environment.GetEnvironmentVariable("UPSTASH_REDIS_TOKEN");
+
+if (string.IsNullOrEmpty(redisToken))
+{
+    throw new InvalidOperationException("Redis token not found in environment variables.");
+}
+
+builder.Services.AddSingleton<IConnectionMultiplexer>(sp =>
+{
+    var configuration = new ConfigurationOptions
+    {
+        EndPoints = { redisUrl }, 
+        Password = redisToken,   
+        Ssl = true,              
+        AbortOnConnectFail = false
+    };
+
+    return ConnectionMultiplexer.Connect(configuration);
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,6 +31,19 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.MapGet("/redis-test", async (IConnectionMultiplexer redis) =>
+{
+    var db = redis.GetDatabase();
+
+    // Set a value
+    await db.StringSetAsync("foo", "bar");
+
+    // Get the value
+    var value = await db.StringGetAsync("foo");
+    return Results.Ok(new { Key = "foo", Value = value.ToString() });
+});
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
