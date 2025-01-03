616d4766689e9a610902493cff22a1af003caeb9
Twila [Hyper] Habab <twila.shayne.habab@outlook.com>
2025-01-03 15:08:49 +0000

    Added color generator

---
+++ Utils/ColorGenerator.cs
@@ -0,0 +1,13 @@
+﻿namespace Project_A_Server.Utils
+{
+    public class ColorGenerator
+    {
+        public static string GenerateColor()
+        {
+            Random random = new Random();
+            string color = $"#{random.Next(0x1000000):X6}";
+
+            return color;
+        }
+    }
+}
